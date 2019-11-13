91a45256b52202020415c2ffa641cd37428f1a50
ASO1801008 <57513140+ASO1801008@users.noreply.github.com>
2019-11-13 12:42:25 +0900

    Update MainActivity.kt

--- app/src/main/java/jp/ac/asojuku/mysize/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/mysize/MainActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     /*pushの練習コメント*/
     /*commit & pushの練習コメント*/
+    /*リモート繁栄のコメント*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
